20e4c420001e2a79d024d5145e21d3cb525b0afb
rostyslav <rostik@telelan.com.ua>
2019-07-18 14:15:18 +0200

    added more calls

--- src/app/services/api/beer.service.ts
+++ src/app/services/api/beer.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { environment } from '../../../environments/environment';
+import { Beer } from '../../../models';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,15 @@ export class BeerService {
 
   constructor(private http: HttpClient) { }
 
-  getBeers(): Observable<any> {
-    return this.http.get(`${environment.baseUrl}/beers`);
+  getBeers(): Observable<Beer[]> {
+    return this.http.get<Beer[]>(`${environment.baseUrl}/beers`);
+  }
+
+  getSingleBeer(): Observable<Beer> {
+    return this.http.get<Beer>(`${environment.baseUrl}/beers/1`);
+  }
+
+  getRandomBeer(): Observable<Beer> {
+    return this.http.get<Beer>(`${environment.baseUrl}/beers/random`);
   }
 }
