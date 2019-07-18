f20d84a197f607385bc78ed7ff8d890dc3f9a022
rostyslav <rostik@telelan.com.ua>
2019-07-18 13:42:09 +0200

    added beer-description component and berr service (empty)

---
+++ src/app/beer-description/beer-description.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'glofox-beer-description',
+  templateUrl: './beer-description.component.html',
+  styleUrls: ['./beer-description.component.scss']
+})
+export class BeerDescriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
