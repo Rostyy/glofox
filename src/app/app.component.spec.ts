bf952485e181bbe088f08181685bf9b993677242
rostyslav <rostik@telelan.com.ua>
2019-07-19 10:14:21 +0200

    fixed unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed, async } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
+import { AppComponent } from './app.component';
 import { BeerDescriptionComponent } from './beer-description/beer-description.component';
 import { BeerListComponent } from './beer-list/beer-list.component';
+import { SearchFieldComponent } from './search-field/search-field.component';
+import { SearchByPipe } from './pipes/search-by.pipe';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -11,10 +14,13 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent,
         BeerDescriptionComponent,
-        BeerListComponent
+        BeerListComponent,
+        SearchFieldComponent,
+        SearchByPipe
       ],
       imports: [
-        HttpClientTestingModule
+        HttpClientTestingModule,
+        ReactiveFormsModule
       ]
     }).compileComponents();
   }));
