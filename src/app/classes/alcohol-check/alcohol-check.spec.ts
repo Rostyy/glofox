7b1045bf8aa2b24e16e45c8cc853fb0dc8ff835b
rostyslav <rostik@telelan.com.ua>
2019-07-21 20:00:19 +0200

    written alcocheck tests

--- src/app/classes/alcohol-check/alcohol-check.spec.ts
+++ src/app/classes/alcohol-check/alcohol-check.spec.ts
@@ -1,7 +1,19 @@
 import { AlcoholCheck } from './alcohol-check';
 
 describe('AlcoholCheck', () => {
+  const alcCheck = new AlcoholCheck();
+
   it('should create an instance', () => {
-    expect(new AlcoholCheck()).toBeTruthy();
+    expect(alcCheck).toBeTruthy();
+  });
+
+  it('should check that 0.05 is non alcoholic abv', () => {
+    const isNonAlcoholic = alcCheck.isNonAlcoholic('0.04');
+    expect(isNonAlcoholic).toBeTruthy();
+  });
+
+  it('should check that 8 is alcoholic abv', () => {
+    const isNonAlcoholic = alcCheck.isNonAlcoholic('8');
+    expect(isNonAlcoholic).toBeFalsy();
   });
 });
